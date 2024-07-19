e2574c48924dac02b6de82df24878920ab5793ab
Sivapragasam <qc.siva@gmail.com>
2024-07-19 17:24:24 +0530

    Edited

--- DOM practice/class 16&18th practice/callbackhellclass/script.js
+++ DOM practice/class 16&18th practice/callbackhellclass/script.js
@@ -2,14 +2,26 @@ function calculateAge(year,callback) {
     setTimeout(()=>{
         let age = 2024 - year
         callback(age)
-    },2000)
-    
+    },2000) 
 }
 
+function canvote(age,callback) {
+    setTimeout(() => {
+        if(age>18){
+            callback (true)
+        } else{
+            callback (false)
+        }
+    }, 3000);
+}
 let personYear = 1988
-calculateAge(personYear,function(birthyear)
+calculateAge(personYear,function(personage)
 {
-    console.log(birthyear)
+    console.log(personage)
+    canvote(personage,function(isEligibleForVote){
+        console.log(isEligibleForVote);
+    })
+    
 })
 
 console.log("other Lines")
