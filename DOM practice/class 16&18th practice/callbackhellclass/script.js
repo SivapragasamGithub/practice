9881a640982ccd7fbcf545ea785b2319de7687fe
Sivapragasam <qc.siva@gmail.com>
2024-07-21 00:26:23 +0530

    edited

--- DOM practice/class 16&18th practice/callbackhellclass/script.js
+++ DOM practice/class 16&18th practice/callbackhellclass/script.js
@@ -14,12 +14,27 @@ function canvote(age,callback) {
         }
     }, 3000);
 }
+
+function provideVoterId(isEligibleForVote,callback) {
+    setTimeout(() => {
+        if (isEligibleForVote == true){
+            callback ("here is your Voter Id") 
+        }
+        else {
+            callback("Sorry Kid")
+        }
+    }, 4000);
+}
+
 let personYear = 1988
 calculateAge(personYear,function(personage)
 {
     console.log(personage)
     canvote(personage,function(isEligibleForVote){
-        console.log(isEligibleForVote);
+        console.log(isEligibleForVote)
+        provideVoterId(isEligibleForVote,function (voterId) {
+            console.log(voterId);
+        })
     })
     
 })
