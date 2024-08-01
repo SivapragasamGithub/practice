39d0a9ef6ff85bb30da61adc43cbc0e01aab17dc
Sivapragasam <qc.siva@gmail.com>
2024-08-01 12:17:17 +0530

    edit

--- REACT DEMO/statedemo/src/Light.jsx
+++ REACT DEMO/statedemo/src/Light.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react"
 
 function Light() {
-    let toggleswitchon = (toggleState) => {
+    
+    const [lightswitch, setswitch] = useState(true)
+    
+    let toggleswitch= (toggleState) => {
         setswitch(toggleState)
     }
 
-    const [lightswitch, setswitch] = useState(true)
+    
     return <div>
         <div className="light" style={{ backgroundColor: !lightswitch ? 'grey' : 'red' }} >
 
         </div>
 
         <button onClick={() => {
-            setswitch(true)
+            toggleswitch (true)
         }}>On</button>
         <button onClick={() => {
-            setswitch(false)
-        }}>Off</button>
+            toggleswitch(false)
+        }}>Off</button> <br></br>
+        <button className="red" >red</button>
+        <button className="red" >green</button>
+        <button className="red" >blue</button><br></br>
+        <button>+</button>
+
+        <button>-</button>
     </div>
+
 }
 export default Light
