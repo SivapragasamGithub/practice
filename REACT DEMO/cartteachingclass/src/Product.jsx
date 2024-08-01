b6ee8204c960dd1cec80f2834732f0ef6b18ae09
Sivapragasam <qc.siva@gmail.com>
2024-08-01 13:03:40 +0530

    edit

---
+++ REACT DEMO/cartteachingclass/src/Product.jsx
@@ -0,0 +1,13 @@
+function Product(params) {
+    return   <div class="card" style={{width:"12rem"}} >
+    <img src="https://fastly.picsum.photos/id/237/200/200.jpg?hmac=zHUGikXUDyLCCmvyww1izLK3R3k8oRYBRiTizZEdyfI" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title" style={{textAlign:"center"}}>Card title</h5>
+      <p class="card-text" style={{textAlign:"center"}}><h5>RS100</h5></p>
+      <button class="btn btn-primary" >Add to Cart</button>
+    </div>
+  </div>
+
+    
+}
+export default Product
