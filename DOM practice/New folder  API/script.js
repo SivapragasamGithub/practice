3fcf3494fa52b3f8123ffee86697c9196d31e2d4
Sivapragasam <qc.siva@gmail.com>
2024-08-04 21:32:07 +0530

    edited

--- DOM practice/New folder  API/script.js
+++ DOM practice/New folder  API/script.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiListContainer = document.getElementById('api-list');
-  
-    fetch('https://api.publicapis.org/entries')
+  const catImagesContainer = document.getElementById('cat-images');
+  const loadMoreButton = document.getElementById('load-more');
+
+  function fetchCatImages() {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=4')
       .then(response => response.json())
       .then(data => {
-        const apis = data.entries;
-        apis.forEach(api => {
-          const apiCard = document.createElement('div');
-          apiCard.className = 'col-md-4';
-          apiCard.innerHTML = `
+        data.forEach(cat => {
+          const catCard = document.createElement('div');
+          catCard.className = 'col-md-3';
+          catCard.innerHTML = `
             <div class="card mb-4">
-              <div class="card-body">
-                <h5 class="card-title">${api.API}</h5>
-                <p class="card-text">${api.Description}</p>
-                <a href="${api.Link}" target="_blank" class="btn btn-primary">Go to API</a>
-              </div>
+              <img src="${cat.url}" class="card-img-top" alt="Random Cat">
             </div>
           `;
-          apiListContainer.appendChild(apiCard);
+          catImagesContainer.appendChild(catCard);
         });
       })
       .catch(error => {
-        console.error('Error fetching API list:', error);
+        console.error('Error fetching cat images:', error);
       });
-  });
-  
+  }
+
+  loadMoreButton.addEventListener('click', fetchCatImages);
+
+  // Initial load
+  fetchCatImages();
+});
