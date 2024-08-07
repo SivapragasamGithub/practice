ece2c5fce884d9b49057ff0e8c042fbbaac1ff41
Sivapragasam <qc.siva@gmail.com>
2024-08-08 02:09:04 +0530

    edited

--- REACT DEMO/mytodolist_task/src/Taskbar.jsx
+++ REACT DEMO/mytodolist_task/src/Taskbar.jsx
@@ -8,15 +8,14 @@ function Taskbar() {
                     <h3> My todo</h3>
                 </div>
                 <div className='col-lg-5' style={{ marginTop: "50px", display: "flex", justifyContent: "end" }}> <h3>Status Filter</h3></div>
-                <div className='col-lg-2' style={{ marginTop: "50px", display: "flex", justifyContent: "end" }}>
+                <div className='col-lg-2' style={{ marginTop: "50px", display: "flex", justifyContent:"flex-start" }}>
                     <div class="dropdown">
                         <button class="btn  dropdown-toggle" style={{backgroundColor:"#ff7f7d"}} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown button
-                        </button>
+All                        </button>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><button class="dropdown-item">All</button></li>
+                            <li><button class="dropdown-item" >completed</button></li>
+                            <li><button class="dropdown-item" >Not completed</button></li>
                         </ul>
                     </div>
                 </div>
