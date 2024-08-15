c81997cc9c2d09d5e682f9c43e58445ad1a0515a
Sivapragasam <qc.siva@gmail.com>
2024-08-15 20:46:01 +0530

    edited

--- REACT DEMO/routerdom_1/src/Card.jsx
+++ REACT DEMO/routerdom_1/src/Card.jsx
@@ -4,7 +4,7 @@ function Card(course) {
     return (
         <div className='col-lg-4' >
             <div class="card" style={{ width: " 27rem" }}>
-                <img src="https://static.guvi.in/blog/zenThumbnail/java-fsd.webp" class="card-img-top" alt="..." />
+                <img src={course.image} class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">{course.name}</h5>
                     <p class="card-text" style={{ marginBottom: "2px" }}>available in</p>
