0e3cd526641e44168f48fe13676821e9d6c9f6ab
Sivapragasam <qc.siva@gmail.com>
2024-08-17 20:24:57 +0530

    form validation done before axios

--- REACT DEMO/got_2/src/Modal.jsx
+++ REACT DEMO/got_2/src/Modal.jsx
@@ -6,54 +6,83 @@ import { useNavigate } from 'react-router-dom'
 function Modal() {
     const navigation = useNavigate()
     const formik = useFormik({
-        initialValues:{
-            name:"",
-            image:"",
-            clan:"",
-            description:""
+        initialValues: {
+            name: "",
+            image: "",
+            clan: "",
+            description: ""
         },
-        onSubmit:()=>{
-            console.log("submit");
-            
+        validate: (values) => {
+            let error = {};
+            if (values.name == "") {
+                error.name = "please enter name"
+            }
+            if (values.image == "") {
+                error.image = "please enter image"
+            }
+            if (values.clan == "") {
+                error.clan = "please enter clan"
+            }
+            if (values.description == "") {
+                error.description = "please enter description"
+            }
 
+
+            return error
+        },
+        onSubmit: (values) => {
+            console.log(values);
         }
-    }) 
-  return (
-    <div class="modal" style={{display:"block"}}>
-  <div class="modal-dialog"  role="document">
-    <div class="modal-content">
-      <form onSubmit={formik.handleSubmit}>
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-      </div>
-      <div class="modal-body">        
-      <div className='col-lg-12' >
-            <label htmlFor="">Name</label>
-            <input type="text" className='form-control' />
+    })
+    return (
+        <div class="modal" style={{ display: "block" }}>
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form onSubmit={formik.handleSubmit}>
+                        <div class="modal-header">
+                            <h5 class="modal-title">Modal title</h5>
+                        </div>
+                        <div class="modal-body">
+                            <div className='col-lg-12' >
+                                <label htmlFor="">Name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={formik.values.name}
+                                    onChange={formik.handleChange}
+                                    className={`form-control ${formik.errors.name && "is-invalid"}`} />
+                            </div>
+                            <div className='col-lg-12' >
+                                <label htmlFor="">Image</label>
+                                <input type="text" name="image"
+                                    value={formik.values.image}
+                                    onChange={formik.handleChange} className={`form-control ${formik.errors.image && "is-invalid"}`} />
+                            </div>
+                            <div className='col-lg-12' >
+                                <label htmlFor="">Clan</label>
+                                <input type="text"
+                                    name="clan"
+                                    value={formik.values.clan}
+                                    onChange={formik.handleChange} className={`form-control ${formik.errors.clan && "is-invalid"}`} />
+                            </div>
+                            <div className='col-lg-12' >
+                                <label htmlFor="">Description</label>
+                                <textarea
+                                    name="description"
+                                    value={formik.values.description}
+                                    onChange={formik.handleChange} className={`form-control ${formik.errors.description && "is-invalid"}`} />
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <input type="submit" class="btn btn-primary" value={"save changes"} />
+                            <button type="button" class="btn btn-secondary" onClick={() => navigation(-1)} data-bs-dismiss="modal">Close</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className='col-lg-12' >
-            <label htmlFor="">Image</label>
-            <input type="text" className='form-control' />
-        </div>
-        <div className='col-lg-12' >
-            <label htmlFor="">Clan</label>
-            <input type="text" className='form-control' />
-        </div>
-        <div className='col-lg-12' >
-            <label htmlFor="">Description</label>
-            <textarea  className='form-control' />
-        </div>        
-      </div>
-      <div class="modal-footer">
-      <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" onClick={()=>navigation(-1)} data-bs-dismiss="modal">Close</button>
-        
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-  )
+    )
 }
 
 export default Modal
