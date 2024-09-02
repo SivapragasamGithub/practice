4024e2382f952903f994644292847bddec5e3cb1
Sivapragasam <qc.siva@gmail.com>
2024-09-03 01:34:25 +0530

    o/p users

--- Nodejs working/practice/index.js
+++ Nodejs working/practice/index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const express = require ("express")
+const app = express()
+
+let users = [
+    {
+        name:"user 1",
+        age:20
+    },
+    {
+        name:"user 2",
+        age:21
+    },
+
+]
+
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+
+app.listen(3000,()=>{
+    console.log("web server is running on port 3000");
+    
+})
